1a1ea68b3303b7bdabd2641f68180a064c8257cc
Adam Zivlak <Adam.zivlak99@hotmail.se>
2023-02-23 12:17:10 +0100

    Sverige, Tyskland, San Marino instansierat

---
+++ Lander/Länder/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Länder
+{
+    internal class Program
+    {
+        public class Land
+        {
+            public string land, styrestyp, huvudstad, invånarantal;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Land sverige = new Land()
+            {
+                land = "Sverige",
+                styrestyp = "monarki",
+                huvudstad = "Stockholm",
+                invånarantal = "10 512 820"
+            };
+            Land tyskland = new Land()
+            {
+                land = "Tyskland",
+                styrestyp = "republik",
+                huvudstad = "Berlin",
+                invånarantal = "83 783 902"
+            };
+            Land san_marino = new Land()
+            {
+                land = "San Marino",
+                styrestyp = "republik",
+                huvudstad = "San Marino",
+                invånarantal = "33 600"
+            };
+            //Console.WriteLine($"Land: {sverige.land}");
+            //Console.WriteLine($"Huvudstad: {sverige.huvudstad}");
+            //Console.WriteLine($"Styrestyp: {sverige.styrestyp}");
+            //Console.WriteLine($"Invånarantal: {sverige.invånarantal} männsikor");
+        }
+    }
+}
